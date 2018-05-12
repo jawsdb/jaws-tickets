f204acc3cf832fd5b7b3c3c096452769a210dd9c
johnpwillman <john@jawsdb.com>
2018-05-11 23:53:37 -0700

    modify config for production env to use mysql

--- config/env/production.js
+++ config/env/production.js
@@ -47,10 +47,10 @@ module.exports = {
     *                                                                          *
     ***************************************************************************/
     default: {
-      // adapter: 'sails-mysql',
-      // url: 'mysql://user:password@host:port/database',
-      adapter: 'sails-mongo',
-      url: process.env['MONGODB_URI'],
+      adapter: 'sails-mysql',
+      url: process.env.JAWSDB_URL,
+      //adapter: 'sails-mongo',
+      //url: process.env['MONGODB_URI'],
       //--------------------------------------------------------------------------
       //  /\   To avoid checking it in to version control, you might opt to set
       //  ||   sensitive credentials like `url` using an environment variable.
